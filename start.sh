e7863708818aa474313096ed58c5497ebc0cc825
Myron <Myron.zhang@coinstore.com>
2023-05-30 00:13:56 +0800

    fix: makefile

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./tendermint start --proxy_app=evmstore --rpc.grpc_laddr=tcp://0.0.0.0:9091
+nohup ./tendermint start --proxy_app=evmstore --rpc.grpc_laddr=tcp://0.0.0.0:9091 > tendermint.log 2>&1 &
